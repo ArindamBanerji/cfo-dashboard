40d26569315595e29469d01add0d4674ecef9329
ArindamBanerji <banerji.arindam@gmail.com>
2024-06-30 19:06:42 -0700

    removed a few extra files

--- code-v2/run_ngrok.py
+++
@@ -1,8 +0,0 @@
-from pyngrok import ngrok 
-# !ngrok authtoken ["cr_2i80nCqqQsXqA1Nw2BRNAhCLHa0"]
-# !nohup streamlit run cfo-dashboard-streamlit-complete.py & 
-
-url = ngrok.connect(port = 8501)
-url #generates our URL
-
-# !streamlit run --server.port 80 cfo-dashboard-streamlit-complete.py >/dev/null #used for starting our server
